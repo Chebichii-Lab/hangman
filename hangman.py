4362aa1fa9fb6ae1ce9f4ce50dafb2692f906abe
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-05 22:41:17 +0300

    get user inputs

--- hangman.py
+++ hangman.py
@@ -15,3 +15,20 @@ def hangman():
     word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase) #alphabet
     used_letters = set() #what the user has guessed
+
+# getting user inputs
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You have already used that character. Please try again.')
+
+    else:
+        print('Invalid character. Please try again')
+
+    
+user_input = input('Type something:')
+print(user_input)
