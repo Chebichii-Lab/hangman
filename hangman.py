a0ab8a1f716956b91b0c6bdefbee22849090741a
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-05 22:26:44 +0300

    create function to get a valid word

--- hangman.py
+++ hangman.py
@@ -1,4 +1,10 @@
 import random
 from words import words
 
-print(words)
+def get_valid_word(words):
+    word = random.choice(words) # takes in a list and randomly chooses something from the list
+
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
