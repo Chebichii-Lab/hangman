ccc9e8567bfbbdff93d031787cdb0e8d56f5cc94
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-06 19:18:29 +0300

    create a while condition to tell the users what they have used

--- hangman.py
+++ hangman.py
@@ -8,7 +8,7 @@ def get_valid_word(words):
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
-    return word.upper()
+    return word 
 
 def hangman():
     word = get_valid_word(words)
@@ -17,17 +17,28 @@ def hangman():
     used_letters = set() #what the user has guessed
 
 # getting user inputs
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0:
+        #letters they have used
+        #' ',join(['a', 'b', 'cd']) -->'a b cd'
+        #.join turns a list into a string
+        print('You have used these letters: ', ''.join(used_letters))
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again.')
+        #what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
 
-    else:
-        print('Invalid character. Please try again')
+        
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again')
 
     
 user_input = input('Type something:')
